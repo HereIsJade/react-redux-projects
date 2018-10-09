6f2030cb5c4001905063e9c4105762723ea8e6b1
HereIsJade <jingfeilin@me.com>
2018-10-09 18:08:29 -0400

    reducer function, action creator and route /

---
+++ blog/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom'
+import promise from 'redux-promise'
+
+import PostsIndex from './components/posts_index'
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Route path='/' component={PostsIndex} />
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
